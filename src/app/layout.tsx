6980ccf5224813ba230e59859d60e07304804c3d
mrman-ahm <manbtd1@gmail.com>
2026-07-26 00:34:35 +0500

    feat: load THROHI V3 optical system after legacy layers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ import "./v2-sector9c-fixes.css";
 import "./v2-sector9d.css";
 import "./v2-sector9d-fixes.css";
 import "./v2-sector9d-refinement.css";
+import "./v3-optical-system.css";
 
 const archivo = Archivo({ subsets: ["latin"], variable: "--font-archivo", display: "swap" });
 const instrument = Instrument_Sans({ subsets: ["latin"], variable: "--font-instrument", display: "swap" });
