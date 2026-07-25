0b51a27db16a102e2f0acd94ffd5046788b7d13a
mrman-ahm <manbtd1@gmail.com>
2026-07-26 00:38:32 +0500

    feat: activate V3 cinematic and hero layer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ import "./v2-sector9d.css";
 import "./v2-sector9d-fixes.css";
 import "./v2-sector9d-refinement.css";
 import "./v3-optical-system.css";
+import "./v3-cinematic-hero.css";
 
 const archivo = Archivo({ subsets: ["latin"], variable: "--font-archivo", display: "swap" });
 const instrument = Instrument_Sans({ subsets: ["latin"], variable: "--font-instrument", display: "swap" });
